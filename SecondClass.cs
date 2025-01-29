fca8b9c5274e06ea2d6c2d48dca4cd3615c42f7e
Olivia Christensen <oliviapc09@gmail.com>
2025-01-29 15:42:28 -0700

    got rid of unnecessary method

--- SecondClass.cs
+++ SecondClass.cs
@@ -18,25 +18,6 @@ public class SecondClass
 
     }
 
-    // receives the board and outputs the board as playing and determines winner
-    public bool PlayGame(char[,] board, int input, char playerChoice)
-    {
-        // check the position where the user is putting their move
-        int row = (input - 1) / 3;
-        int col = (input - 1) % 3;
-
-        // check if the position is valid
-        if (input < 1 || input > 9 || board[row, col] == 'X' || board[row, col] == 'O')
-        {
-            Console.WriteLine("Invalid move. Please try again.");
-            return false; // Move was invalid
-        }
-
-        // Update the board with the player's move
-        board[row, col] = playerChoice;
-        return true; // Move was successful
-    }
-
     // Method to check if there is a winner
     public bool CheckWinner(char[,] board, char player)
     {
